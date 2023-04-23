2bb697fa182df1369ad7eb598404f1520a9818f2
oleh dlubovskyi <oleh.d@delasport.com>
2023-04-23 15:49:14 +0200

    init

---
+++ src/main/java/io/genevjov/enashi/ENashiApplication.java
@@ -0,0 +1,13 @@
+package io.genevjov.enashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENashiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/genevjov/enashi/ENashiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.genevjov.enashi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENashiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
